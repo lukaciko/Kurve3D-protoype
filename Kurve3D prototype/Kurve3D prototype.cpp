cd1e472ef937cd1ba3bea39514c5fcf5162d99ac
LukaCiko <lukacindro@gmail.com>
2013-01-03 22:47:26 +0100

    Two VAOs, dynamically create circle

--- Kurve3D prototype/Kurve3D prototype.cpp
+++ Kurve3D prototype/Kurve3D prototype.cpp
@@ -15,6 +15,9 @@
 #include <glm/gtc/type_ptr.hpp>
 #include <vector>
 
+#define _USE_MATH_DEFINES // So we can use M_PI
+#include <math.h>
+
 // Shader sources
 const char* vertexSource =
     "#version 150\n"
@@ -50,8 +53,27 @@ const char* fragmentSource =
     "   outColor = texture(tex, TexCoord) * vec4( 1.0, color, color, 1.0 );"
     "}";
 
+// Shader sources for cylinders
+const char* cylinderVertexSource =
+    "#version 150\n"
+    "in vec2 position;"
+    "uniform mat4 model;"
+    "uniform mat4 view;"
+    "uniform mat4 proj;"
+    "void main() {"
+    "	gl_Position = proj * view * model * vec4( position, 0.0, 1.0 );"
+    "}";
+const char* cylinderFragmentSource =
+    "#version 150\n"
+    "out vec4 outColor;"
+    "void main() {"
+    "   outColor = vec4( 1.0f, 0.0f, 1.0f, 1.0f);"
+    "}";
+
 GLuint vertexShader;
 GLuint fragmentShader;
+GLuint cylinderVertexShader;
+GLuint cylinderFragmentShader;
 
 void GLFWCALL keyCallbackFunction(int key, int newState) {
     std::cout << key << " " << ( newState == GLFW_PRESS ? "pressed" : "released" ) << "\n";
@@ -67,41 +89,41 @@ float vertices[] = {
     -0.1f, +0.1f, 0.0f, 1.0f // Vertex 3 (X, Y)
 };
 
-GLuint compileShaders(){
+GLuint compileShaders(GLuint aVertexShader, GLuint aFragmentShader, const char * aVertexSource, const char * aFragmentSource){
     // Create and compile the vertex shader
-    vertexShader = glCreateShader( GL_VERTEX_SHADER );
-    glShaderSource( vertexShader, 1, &vertexSource, NULL );
-    glCompileShader( vertexShader );
+    aVertexShader = glCreateShader( GL_VERTEX_SHADER );
+    glShaderSource( aVertexShader, 1, &aVertexSource, NULL );
+    glCompileShader( aVertexShader );
 
     // Create and compile the fragment shader
-    fragmentShader = glCreateShader( GL_FRAGMENT_SHADER );
-    glShaderSource( fragmentShader, 1, &fragmentSource, NULL );
-    glCompileShader( fragmentShader );
+    aFragmentShader = glCreateShader( GL_FRAGMENT_SHADER );
+    glShaderSource( aFragmentShader, 1, &aFragmentSource, NULL );
+    glCompileShader( aFragmentShader );
 
     // Link the vertex and fragment shader into a shader program
     GLuint shaderProgram = glCreateProgram();
-    glAttachShader( shaderProgram, vertexShader );
-    glAttachShader( shaderProgram, fragmentShader );
+    glAttachShader( shaderProgram, aVertexShader );
+    glAttachShader( shaderProgram, aFragmentShader );
     glBindFragDataLocation( shaderProgram, 0, "outColor" );
     glLinkProgram( shaderProgram );
 
     GLint abort = 0;
     GLint testVal;
     //catch any errors
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &testVal);
+    glGetShaderiv(aVertexShader, GL_COMPILE_STATUS, &testVal);
     if(testVal == GL_FALSE)
     {
         abort = 1;
         char infolog[1024];
-        glGetShaderInfoLog(vertexShader,1024,NULL,infolog);
+        glGetShaderInfoLog(aVertexShader,1024,NULL,infolog);
         std::cerr << "The vertex shader failed to compile with the error:" << infolog;
     }
 
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &testVal);
+    glGetShaderiv(aFragmentShader, GL_COMPILE_STATUS, &testVal);
     if(testVal == GL_FALSE) {
         abort = 1;
         char infolog[1024];
-        glGetShaderInfoLog(fragmentShader,1024,NULL,infolog);
+        glGetShaderInfoLog(aFragmentShader,1024,NULL,infolog);
         std::cerr << "The fragment shader failed to compile with the error:" << infolog;
     }
 
@@ -153,17 +175,21 @@ int _tmain(int argc, _TCHAR* argv[])
 
     glfwSetKeyCallback( &keyCallbackFunction );
 
+    GLuint VAOs [2];
+    glGenVertexArrays( 2, VAOs );
+    glBindVertexArray( VAOs[0] );
+
     GLuint circleTex;
-    glGenTextures( 1, &circleTex );   
+    glGenTextures( 1, &circleTex );
     glBindTexture( GL_TEXTURE_2D, circleTex );
 
     int iWidth, iHeight;
     unsigned char* image;
-    
+
     const char* path = "circle.png";
     image = SOIL_load_image( path, &iWidth, &iHeight, 0, SOIL_LOAD_RGBA );
-	glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA, iWidth, iHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, image );
-	SOIL_free_image_data( image );
+    glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA, iWidth, iHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, image );
+    SOIL_free_image_data( image );
 
     glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT );
     glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT );
@@ -176,9 +202,9 @@ int _tmain(int argc, _TCHAR* argv[])
     glBindBuffer( GL_ARRAY_BUFFER, vbo );
     glBufferData( GL_ARRAY_BUFFER, sizeof( vertices ), vertices, GL_STATIC_DRAW );
 
-    GLuint shaderProgram = compileShaders();
+    GLuint shaderProgram = compileShaders(vertexShader, fragmentShader, vertexSource, fragmentSource);
     glUseProgram( shaderProgram );
-    
+
     GLint posAttrib = glGetAttribLocation( shaderProgram, "position" );
     glVertexAttribPointer( posAttrib, 2, GL_FLOAT, GL_FALSE, 4*sizeof(float), 0 );
     glEnableVertexAttribArray( posAttrib );
@@ -187,18 +213,50 @@ int _tmain(int argc, _TCHAR* argv[])
     glVertexAttribPointer( texAttrib, 2, GL_FLOAT, GL_FALSE, 4*sizeof(float), (void * ) ( 2*sizeof(float) ) );
     glEnableVertexAttribArray( texAttrib );
 
-	glEnable( GL_DEPTH_TEST );
+    glEnable( GL_DEPTH_TEST );
 
     Snake* p_snake = new Snake;
     std::vector<SnakeLink *> snakeLinks;
+
+    // Set up cylinder drawing
+    glBindVertexArray( VAOs[1] );
+
+    // Generate the circle vertices
+    int k = 12;
+    float r = 0.25;
+    float* circleVertices =  new float[2*k];
+    
+    for(int i = 0; i != k; ++i) {
+        float phi = 2 * M_PI * i / k;
+        float x = sin(phi) * r;
+        float y = cos(phi) * r;
+        circleVertices[2*i] = x;
+        circleVertices[2*i+1] = y;
+    }
+
+    GLuint circleVBO;
+    glGenBuffers( 1, &circleVBO ); // Generate 1 buffer
+    glBindBuffer( GL_ARRAY_BUFFER, circleVBO );
+    glBufferData( GL_ARRAY_BUFFER, 2 * k * sizeof(float), circleVertices, GL_STATIC_DRAW );
+
+    GLuint cylinderShaderProgram = compileShaders(cylinderVertexShader, cylinderFragmentShader, cylinderVertexSource, cylinderFragmentSource);
+    glUseProgram( cylinderShaderProgram );
+
+    GLint cyposAttrib = glGetAttribLocation( cylinderShaderProgram, "position" );
+    glVertexAttribPointer( cyposAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0 );
+    glEnableVertexAttribArray( cyposAttrib );
 
     // Main game loop
     while( glfwGetWindowParam( GLFW_OPENED ) ) {
 
         glClearColor( 0.0f, 0.0f, 0.0f, 1.0f );
-		glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
-		bool crash=false;
-		//if (glfwGetKey( 'a' ) == GLFW_PRESS || glfwGetKey( 'A' ) == GLFW_PRESS)
+        glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+
+        glBindVertexArray(VAOs[0]);
+        glUseProgram( shaderProgram );
+
+        bool crash=false;
+        //if (glfwGetKey( 'a' ) == GLFW_PRESS || glfwGetKey( 'A' ) == GLFW_PRESS)
         //    view = glm::rotate( view, 0.3f, glm::vec3( 1.0f, 0.0f, 0.0f) );
         //if (glfwGetKey( 'b' ) == GLFW_PRESS || glfwGetKey( 'B' ) == GLFW_PRESS)
         //    view = glm::rotate( view, 0.3f, glm::vec3( 0.0f, 1.0f, 0.0f) );
@@ -223,27 +281,27 @@ int _tmain(int argc, _TCHAR* argv[])
 
         glm::vec3 sPos = p_snake->getPosition();
         double dist= 0;
-		double increase=0.001;
-		bool change = false;
-		int i=0;
-		bool collision = false;
-		for(std::vector<SnakeLink *>::iterator it = snakeLinks.begin(); it != snakeLinks.end(); ++it) {
+        double increase=0.001;
+        bool change = false;
+        int i=0;
+        bool collision = false;
+        for(std::vector<SnakeLink *>::iterator it = snakeLinks.begin(); it != snakeLinks.end(); ++it) {
             i++;
-			if (dist>0.899 & !change) {
-				increase *= -1;
-				change = true;
-			}
-			if (dist<0.05 & change) {
-				increase *= -1;
-				change = false;
-			}
-			dist=dist+increase;
-			glm::vec3 iPos = (*it)->getPosition();
+            if (dist>0.899 & !change) {
+                increase *= -1;
+                change = true;
+            }
+            if (dist<0.05 & change) {
+                increase *= -1;
+                change = false;
+            }
+            dist=dist+increase;
+            glm::vec3 iPos = (*it)->getPosition();
             if((pow(sPos.x-iPos.x,2) < 0.01) && (pow(sPos.y-iPos.y,2) < 0.01) && (pow(sPos.z-iPos.z,2) < 0.01) && (i<(int)snakeLinks.size()-150) && ((int)snakeLinks.size()>150) && !collision){
-				 std::cout<<"Collision!";
-				 collision=true;
-			}
-			GLint uniModel = glGetUniformLocation( shaderProgram, "model" );
+                std::cout<<"Collision!";
+                collision=true;
+            }
+            GLint uniModel = glGetUniformLocation( shaderProgram, "model" );
             glUniformMatrix4fv( uniModel, 1, GL_FALSE, glm::value_ptr( glm::translate(glm::mat4(), iPos ) ) );
 
             glm::vec3 distV = sPos - iPos;
@@ -264,11 +322,22 @@ int _tmain(int argc, _TCHAR* argv[])
 
         glDrawArrays( GL_TRIANGLES, 0, 6 );
 
+        glBindVertexArray(VAOs[1]);
+        glUseProgram( cylinderShaderProgram );
 
+        GLint uniModel1 = glGetUniformLocation( cylinderShaderProgram, "model" );
+        glUniformMatrix4fv( uniModel1, 1, GL_FALSE, glm::value_ptr( glm::translate(glm::mat4(), sPos ) ) );
+        GLint uniView1 = glGetUniformLocation( cylinderShaderProgram, "view" );
+        glUniformMatrix4fv( uniView1, 1, GL_FALSE, glm::value_ptr( view ) );
+        glm::mat4 proj1 = glm::perspective( 45.0f, (float)width / (float)height, 1.0f, 10.0f );
+        GLint uniProj1 = glGetUniformLocation( cylinderShaderProgram, "proj" );
+        glUniformMatrix4fv( uniProj1, 1, GL_FALSE, glm::value_ptr( proj1 ) );
+
+        glDrawArrays( GL_LINE_LOOP, 0, k   );
         glfwSwapBuffers(); // Also calls glfwPoolEvents
     }
 
-    //delete snakelinks
+    // TODO delete snakelinks
 
     delete p_snake;
     p_snake = 0;
